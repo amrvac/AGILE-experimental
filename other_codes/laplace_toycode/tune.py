53064f04aff42b393477bd8fcd0407e560e98545
Leon Oostrum <l.oostrum@esciencecenter.nl>
2024-06-11 12:26:56 +0200

    Fix user_dimensions in tuning script

--- other_codes/laplace_toycode/tune.py
+++ other_codes/laplace_toycode/tune.py
@@ -28,19 +28,16 @@ if __name__ == '__main__':
     app = Code(OpenACC(), Fortran())
     preprocessor = extract_preprocessor(code)
 
-    # add user dims to preprocessor
-    for k, v in sizes.items():
-        preprocessor.append(f"#define {k} {v}")
-
     signature = extract_directive_signature(code, app)
     body = extract_directive_code(code, app)
     # Allocate memory on the host
     data = extract_directive_data(code, app)
     init = extract_initialization_code(code, app)
-    args = allocate_signature_memory(data[name], preprocessor) #, user_dimensions=sizes)
+    args = allocate_signature_memory(data[name], preprocessor, user_dimensions=sizes)
     # Generate kernel string
     kernel_string = generate_directive_function(
-        preprocessor, signature[name], body[name], app, data=data[name], initialization=init
+        preprocessor, signature[name], body[name], app, data=data[name], initialization=init,
+        user_dimensions=sizes
     )
     # print("preprocessor:")
     # print(preprocessor)
@@ -51,9 +48,9 @@ if __name__ == '__main__':
     # print("body:")
     # print(body[name])
     # print()
-    print("kernel string:")
-    print(kernel_string)
-    print()
+    # print("kernel string:")
+    # print(kernel_string)
+    # print()
 
     tune_params = {'collapse_factor': [1, 2, 3],
                    'vlength': [2**i for i in range(0, 11, 2)]}
